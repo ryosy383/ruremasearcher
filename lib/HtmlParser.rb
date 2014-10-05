de81579976d00157f11bbe062f397ad50e24fc0f
ryosy383 <mosso.ryosy383@gmail.com>
2014-10-05 17:02:07 +0900

    output search result.

--- lib/HtmlParser.rb
+++ lib/HtmlParser.rb
@@ -1,10 +1,17 @@
 require 'nokogiri'
 
 class HtmlParser
-  attr_accessor :doc
+  attr_accessor :doc, :titles, :metadata, :summaries, :descriptions
 
   def initialize(html)
-    @doc = Nokogiri::HTML::DocumentFragment.parse(html)
+    @doc = Nokogiri::HTML.parse(html)
+  end
+
+  def parse
+    @titles = @doc.css('dt > h3')
+    @metadata =  @doc.css('ul.entry-metadata > li.entry-type > span')
+    @summaries = @doc.css('div.entry-summary > p')
+    @descriptions = @doc.css('div.entry-document > div.entry-snippets > div.snippet')
   end
 
 end
