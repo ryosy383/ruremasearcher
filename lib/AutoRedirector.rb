c3280fda6fa54332973f6dcd80b0ba4a308268a0
ryosy383 <mosso.ryosy383@gmail.com>
2014-10-05 15:03:05 +0900

    Implement AutoRedirector.

--- lib/AutoRedirector.rb
+++ lib/AutoRedirector.rb
@@ -3,12 +3,26 @@
 require 'mechanize'
 
 class AutoRedirector
-  attr_accessor :mechanize
+  attr_accessor :agent
 
   def initialize(agent)
-    @mechanize = Mechanize.new { |a|
+    self.set_agent(agent)
+  end
+
+  def set_agent(agent)
+    @agent = Mechanize.new do |a|
       a.user_agent_alias = agent
-    }
+    end
+  end
+
+  def get(url)
+    @agent.get(url)
+  end
+
+  def search(words)
+    form = @agent.page.forms[0]
+    form.fields[0].value = words
+    form.submit
   end
 
 end
--- lib/ruremasearcher.rb
+++ lib/ruremasearcher.rb
@@ -1,4 +1,4 @@
-require "ruremasearcher/version"
+require '../lib/ruremasearcher/version'
 
 module Ruremasearcher
   # Your code goes here...
